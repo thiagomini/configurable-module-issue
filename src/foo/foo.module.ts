7866aed4ff0580d58b40458fa91421914fcc11af
Thiago Martins <thiago_valentim1@hotmail.com>
2022-09-15 16:36:41 -0300

    fix: add providers from parent module

--- src/foo/foo.module.ts
+++ src/foo/foo.module.ts
@@ -1,4 +1,4 @@
-import { DynamicModule, Module } from '@nestjs/common';
+import { DynamicModule, Global, Module } from '@nestjs/common';
 import { BarModule } from '../bar/bar.module';
 import {
   ASYNC_OPTIONS_TYPE,
@@ -7,6 +7,7 @@ import {
   MODULE_OPTIONS_TOKEN,
 } from './foo-module-definition';
 
+@Global()
 @Module({})
 export class FooModule extends ConfigurableModuleClass {
   static registerAsync(options: typeof ASYNC_OPTIONS_TYPE): DynamicModule {
@@ -14,6 +15,7 @@ export class FooModule extends ConfigurableModuleClass {
     moduleDefinition.imports?.push(
       BarModule.registerAsync({
         inject: [MODULE_OPTIONS_TOKEN],
+        provideInjectionTokensFrom: moduleDefinition.providers,
         useFactory(options) {
           const optionsAsFooModuleOptions = options as FooModuleOptions;
           return {
